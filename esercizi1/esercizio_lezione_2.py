71dcfb1fe0caa48a2a79399586584d0df7eecc11
Replit user <>
2022-11-15 17:43:18 +0000

    esercizio_lezione_2.py

---
+++ esercizi1/esercizio_lezione_2.py
@@ -0,0 +1,15 @@
+#scrivere una funzione sum_list che sommi tutti gli elementi di una lista
+
+def sum_list(item):
+    somma = 0
+    
+    if not number_list:
+        return None 
+    else for item in number_list:
+        somma = somma + item
+    return somma
+
+number_list = [1, 5, 7, 12, 14, 9]
+somma = sum_list(number_list)
+print(somma)
+
