97ee28c238d9290dfc4adbab78ce0ecf521b8696
Paola D'Incecco <114514950+paoladincecco@users.noreply.github.com>
2022-11-17 14:01:54 +0000

    prova 2

--- esercizi1/esercizio_lezione_3.py
+++ esercizi1/esercizio_lezione_3.py
@@ -1,19 +1,10 @@
-def sum_csv(argomento1):
+def sum_csv(file_name):
     somma = 0
-    
-    for item in argomento1:
-        somma = somma + item
-    return somma
+    file_name = open(file_name, 'r')
+    for line in file_name:
+        elements = line.split(',')
+        if elements[0] != 'Date':
+            value = float(elements[1])
+            somma = somma + value
+        return somma
 
-vendite = []
-my_file = open('shampoo_sales.csv', 'r')
-
-for line in my_file:
-    elements = line.split(',')
-    if elements[0] != 'Date':
-        date = elements[0]
-        value = float(elements[1])
-        vendite.append(value)
-
-valori_vendite = sum_csv(vendite)
-print('La somma delle vendite è: {}'.format(valori_vendite))
